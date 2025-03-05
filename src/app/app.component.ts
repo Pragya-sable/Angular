fff3b8ed2879b294ef46991d391ad6186f5ebc85
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-03-05 22:12:35 +0530

    [Pragya] Add. Implemented One-Way Data Binding using Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Helloworld';
+  message: string = "Hello from BridgeLabz ";  // Define message property
 }
