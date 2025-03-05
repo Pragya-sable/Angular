b81a6889a82829860fdd12d94ec9c536d6be0e22
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-03-05 22:47:06 +0530

    [Pragya] Add. Adding Bridgelabz logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,9 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   message: string = "Hello from BridgeLabz ";  // Define message property
+  logoPath: string = "./assets/BL_logo_square_jpg.jpg";  
 }
