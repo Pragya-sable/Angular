4621420881513f00db6fd0dc132d68b97f349f64
Pragya-sable <141444273+Pragya-sable@users.noreply.github.com>
2025-03-05 23:16:54 +0530

    [Pragya] Add. Adding user inputted name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule] ,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true
 })
 export class AppComponent {
   message: string = "Hello from BridgeLabz ";  // Define message property
-  logoPath: string = "./assets/BL_logo_square_jpg.jpg";  
+  logoPath: string = "./assets/BL_logo_square_jpg.jpg"; 
+  userName: string = "";
+ 
 
   openBridgeLabz() {
     window.open("https://www.bridgelabz.com", "_blank");
